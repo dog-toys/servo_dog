ced9510d697d5617d0287d1f9638e5356d38e7d2
siria <siria55lee@gmail.com>
2024-03-22 17:39:12 +0800

    update

--- servo_dog/src/main.cpp
+++ servo_dog/src/main.cpp
@@ -1,128 +1,95 @@
 #include <Arduino.h>
-#include <ESP32Servo.h>
-Servo s1, s2, s3, s4;
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "actions.h"
 
-void s1_1() {
-  s1.write(0);
+// 网络配置
+const char* ssid = "Lemon-Dev";
+const char* password = "lagrengedev";
+
+// MQTT服务器配置
+const char* mqtt_server = "10.25.5.110";
+const int mqtt_port = 1883; // 通常是1883端口，但你的服务器配置可能不同
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  // 连接WiFi网络
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void s1_up() {
-  s1.write(45);
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  String message;
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  Serial.println(message);
+  if (String(topic) == "dog_in_actions") {
+    if (message == "forward") {
+        forward();
+    } else if (message == "back") {
+    } else if (message == "turn_left") {
+
+    } else if (message == "turn_right") {
+
+    } else if (message == "stop") {
+        down();
+    }
+  }
 }
 
-void s1_down() {
-  s1.write(90);
+void reconnect() {
+  // 循环直到连接成功
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // 尝试连接
+    if (client.connect("dog1", "siria", "z13547842355")) {
+      Serial.println("connected");
+      // 一旦连接，发布一个公告...
+      client.publish("outTopic", "hello world");
+      // ...并重新订阅
+      client.subscribe("dog_in_actions");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // 等待5秒后重试
+      delay(5000);
+    }
+  }
 }
-
-void s1_back() {
-  s1.write(135);
-}
-
-void s1_5() {
-  s1.write(180);
-}
-
-void s2_1() {
-  s2.write(180);
-}
-
-void s2_up() {
-  s2.write(135);
-}
-
-void s2_down() {
-  s2.write(90);
-}
-
-void s2_back() {
-  s2.write(45);
-}
-
-void s2_5() {
-  s2.write(0);
-}
-
-void s3_1() {
-  s3.write(0);
-}
-
-void s3_up() {
-  s3.write(45);
-}
-
-void s3_down() {
-  s3.write(90);
-}
-
-void s3_back() {
-  s3.write(135);
-}
-
-void s3_5() {
-  s3.write(180);
-}
-
-void s4_1() {
-  s4.write(180);
-}
-
-void s4_up() {
-  s4.write(135);
-}
-
-void s4_down() {
-  s4.write(90);
-}
-
-void s4_back() {
-  s4.write(45);
-}
-
-void s4_5() {
-  s4.write(0);
-}
-
-
 
 void setup() {
-  Serial.begin(115200);
-  s1.attach(18);
-  s2.attach(17);
-  s3.attach(26);
-  s4.attach(27);
+    Serial.begin(115200);
+    s1.attach(18);
+    s2.attach(17);
+    s3.attach(26);
+    s4.attach(27);
 
-  s1_down();
-  s2_down();
-  s3_down();
-  s4_down();
+    down();
+
+    setup_wifi();
+    client.setServer(mqtt_server, mqtt_port);
+    client.setCallback(callback);
 }
 
 void loop() {
-  s1_up();
-  s4_up();
-  delay(100);
-  s2_back();
-  s3_back();
-  delay(100);
-  s1_down();
-  s4_down();
-  delay(100);
-  s2_down();
-  s3_down();
-  delay(100);
-
-  s2_up();
-  s3_up();
-  delay(100);
-  s1_back();
-  s4_back();
-  delay(100);
-  s2_down();
-  s3_down();
-  delay(100);
-  s1_down();
-  s4_down();
-  delay(100);
-
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 }
 
